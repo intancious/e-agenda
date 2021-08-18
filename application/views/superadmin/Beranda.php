743cbf34783e6c744fc60df9a4b89c912446b915
rosidf26 <frosid26@gmail.com>
2021-08-18 10:47:58 +0700

    tampilan filter

--- application/views/superadmin/Beranda.php
+++ application/views/superadmin/Beranda.php
@@ -8,86 +8,94 @@
         <h1 class="h3 mb-0 text-gray-800">Beranda</h1>
     </div>
 
-    <!-- Content Row -->
     <div class="row">
+        <div class="col-md-12">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Accordion -->
+                <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                    <h6 class="m-0 font-weight-bold text-primary">Filter Berdasarkan</h6>
+                </a>
+                <!-- Card Content - Collapse -->
+                <div class="collapse show" id="collapseCardExample">
+                    <div class="card-body">
+                        <form>
+                            <div class="row">
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Tanggal Kegiatan</label>
+                                        <input class="form-control" type="date" name="tanggal_kegiatan" id="tanggal_kegiatan" tabindex="1">
+                                        <span class="help-block" style="color: red;"></span>
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Agenda</label>
+                                        <select name="agenda" id="agenda" class="form-control" tabindex="3">
+                                            <option value="" disabled selected>--- Pilih ---</option>
+                                            <option value="Bupati">Bupati</option>
+                                            <option value="Wakil Bupati">Wakil Bupati</option>
+                                        </select>
+                                        <span class="help-block" style="color: red;"></span>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                        </div>
+
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-lg-6">
+
+            <!-- Basic Card Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h6 class="m-0 font-weight-bold text-primary">18 Agustus 2021</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">08.00 WIB</h6>
                         </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                        <div class="col-md-6 keterangan">
+                            <small class="label label-success"> Sedang Berlangsung </small>
                         </div>
+
                     </div>
+                </div>
+
+
+                <div class="card-body">
+                    The styling for this basic card example is created by using default Bootstrap
+                    utility classes. By using utility classes, the style of the card component can be
+                    easily modified with no need for any custom CSS!
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-6">
+            <!-- Basic Card Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h6 class="m-0 font-weight-bold text-primary">18 Agustus 2021</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">08.00 WIB</h6>
+                        </div>
+                        <div class="col-md-6 keterangan">
+                            <small class="label label-success"> Sedang Berlangsung </small>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="card-body">
+                    The styling for this basic card example is created by using default Bootstrap
+                    utility classes. By using utility classes, the style of the card component can be
+                    easily modified with no need for any custom CSS!
                 </div>
             </div>
         </div>
