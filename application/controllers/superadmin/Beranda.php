743f8791732a507daec9d1c5050e15a4ecc63b74
rosidf26 <frosid26@gmail.com>
2021-08-20 14:01:12 +0700

    cek login

--- application/controllers/superadmin/Beranda.php
+++ application/controllers/superadmin/Beranda.php
@@ -3,7 +3,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Beranda extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('Auth');
 
+        $auth = new Auth();
+        $auth->is_logged_in();
+    }
 
     public function index()
     {
